e2bda74c880cb29b67f8ccdcb31be0640b6f80ac
Jared Boehm <me@jaredboehm.com>
2023-11-29 16:06:38 -0700

    jwt

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,38 +1,46 @@
 // from activity 26
-const { GraphQLError } = require('graphql')
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const {GraphQLError} = require('graphql');
 
-const secret = 'mysecretssshhhhhhh'
-const expiration = '2h'
+// set token secret and expiration date
+const secret = 'testingsecret';
+const expiration = '2h';
 
 module.exports = {
-    AuthenticationError: new GraphQLError(`Couldn't authenticate user.`, {
-        extensions: {
-            code: 'UNAUTHENTICATED',
-        },
-    }),
-    authMiddleware: function ({ req }) {
-        let token = req.body.token || req.query.token || req.headers.authorization
+  // function for our authenticated routes
+  authMiddleware: function ({req}) {
+    // allows token to be sent via  req.query or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-        if (req.headers.authorization) {
-            token = token.split(' ').pop().trim()
-        }
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
 
-        if (!token) {
-            return req
-        }
+    if (!token) {
+      return req;
+    }
 
-        try {
-            const { data } = jwt.verify(token, secret, { maxAge: expiration });
-            req.user = data
-        } catch {
-            console.log('Invalid token')
-        }
+    // verify token and get user data out of it
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch (err) {
+      console.log('Invalid token:');
+      console.error(err);
+    }
 
-        return req
+    return req;
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+  //error to throw if the user is not auth'd
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
     },
-    signToken: function ({ email, username, _id }) {
-        const payload = { email, username, _id }
-        return jwt.sign({ data: payload }, secret, { expiresIn: expiration })
-    },
-}
+  }),
+};
