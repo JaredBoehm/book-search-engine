c7548bb239e417fe717c73257ca13be9e3c802de
Jared Boehm <me@jaredboehm.com>
2023-11-29 16:50:39 -0700

    saveBook typedef fix

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,10 +26,19 @@ const typeDefs = `
         me: User
     }
 
+    input BookInput {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(authors: [String], description: String, title: String, bookId: String, image: String, link: String): User
+        saveBook(bookData: BookInput!): User
         removeBook(bookId: String): User
     }
 `
