ad408bca5a9d2332cb63c07154a9c6c21265d8ed
Jared Boehm <me@jaredboehm.com>
2023-11-29 15:56:16 -0700

    prod fix3

--- server/server.js
+++ server/server.js
@@ -26,6 +26,10 @@ const startApolloServer = async () => {
     }))
 
     app.use(express.static(path.join(__dirname, '../client/dist')))
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+    })
 
     db.once('open', () => {
         app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`))
