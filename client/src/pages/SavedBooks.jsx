2489cee86be00dfbfa3178d96d39dfdbe747e39e
Jared Boehm <me@jaredboehm.com>
2023-11-29 17:25:55 -0700

    delete book fix

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -34,6 +34,7 @@ const SavedBooks = () => {
 
             // upon success, remove book's id from localStorage
             removeBookId(bookId)
+            window.location.reload()
         } catch (err) {
             console.error(err)
         }
